8da1ce7b5d015793f00fed3db634dbe39d317ca0
ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-06-13 02:16:50 +0800

    add htaccess (trust issue)

--- app/core/App.php
+++ app/core/App.php
@@ -4,6 +4,18 @@ class App
 {
     public function __construct()
     {
-        echo "haii kamu hebat";
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL()
+    {
+        if(isset($_GET["url"]))
+        {
+            $url = rtrim($_GET["url"], '/');
+            $url =  filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
