9bb31634f1e69c1bb68f2fe9715b3659947b9e48
ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-06-15 11:32:52 +0800

    add page teacher absensi and rekap absensi

--- app/controllers/Teacher.php
+++ app/controllers/Teacher.php
@@ -10,4 +10,22 @@ class Teacher extends Controller
         $this->view("teacher/index");
         $this->view("templates/footer");
     }
+
+    public function absensi()
+    {
+        $data['title'] = 'Halaman Absensi';
+
+        $this->view("templates/header", $data);
+        $this->view("teacher/absensi/index");
+        $this->view("templates/footer");
+    }
+
+    public function rekap()
+    {
+        $data['title'] = 'Halaman Rekap Absensi';
+
+        $this->view("templates/header", $data);
+        $this->view("teacher/rekap/index");
+        $this->view("templates/footer");
+    }
 }
