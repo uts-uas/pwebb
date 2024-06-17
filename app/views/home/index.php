47e5241bd838a2d5667f72dd606eeca718e141fd
ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-06-17 17:05:43 +0800

    addjustment navbar

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,67 +1,31 @@
 <body id="page-top">
     <!-- Topbar -->
-    <nav class="navbar navbar-expand navbar-light bg-white topbar  static-top shadow">
-
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top shadow">
         <!-- Sidebar Toggle (Topbar) -->
-        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+        <button id="sidebarToggleTop" class="btn btn-link d-lg-none rounded-circle mr-3">
             <i class="fa fa-bars"></i>
         </button>
-
-        <!-- logo -->
-        <div class="mx-3">
+        <!-- Logo -->
+        <a class="navbar-brand mx-3" href="#">
             <img src="<?= BURL ?>/img /logo.png" alt="" width="60px">
+        </a>
+        <!-- Navbar Links -->
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto align-items-center">
+                <li class="nav-item mr-3">
+                    <a class="nav-link text-primary active font-weight-bold" href="#home">Home</a>
+                </li>
+                <li class="nav-item mr-3">
+                    <a class="nav-link text-primary" href="#willbehappywithyou">About</a>
+                </li>
+                <li class="nav-item mr-3">
+                    <a class="nav-link text-primary" href="#howdoyoufeelstudywithyou">Other</a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn btn-primary font-weight-bold" href="<?= BURL ?>/auth/login">Login Now</a>
+                </li>
+            </ul>
         </div>
-
-        <!-- Topbar Navbar -->
-        <ul class="navbar-nav ml-auto">
-
-            <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-            <li class="nav-item dropdown no-arrow d-sm-none">
-                <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="fas fa-search fa-fw"></i>
-                </a>
-                <!-- Dropdown - Messages -->
-                <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
-                    <form class="form-inline mr-auto w-100 navbar-search">
-                        <div class="input-group">
-                            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-                            <div class="input-group-append">
-                                <button class="btn btn-primary" type="button">
-                                    <i class="fas fa-search fa-sm"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </li>
-
-            <!-- Nav Item - Alerts -->
-            <li class="nav-item">
-                <a class="nav-link text-primary active font-weight-bold " href="#">Home</a>
-            </li>
-
-            <!-- Nav Item - Alerts -->
-            <li class="nav-item">
-                <a class="nav-link text-primary" href="#willbehappywithyou">About</a>
-            </li>
-
-            <!-- Nav Item - Alerts -->
-            <li class="nav-item">
-                <a class="nav-link text-primary" href="#howdoyoufeelstudywithyou">other</a>
-            </li>
-
-
-
-            <div class="topbar-divider d-none d-sm-block"></div>
-
-            <!-- Nav Item - Alerts -->
-            <li class="nav-item my-3 mx-3">
-                <a class="btn btn-primary font-weight-bold" href="<?= BURL ?>/auth/login">
-                    Login Now
-                </a>
-            </li>
-
-        </ul>
     </nav>
     <!-- End of Topbar -->
 
@@ -70,7 +34,7 @@
         <!-- LEARN MAKE YOU HAPPY -->
         <div class="container my-5">
             <div class="d-flex justify-content-around align-items-center">
-                <div class="text-left w-50">
+                <div class="text-left w-50" data-aos="fade-right">
                     <div class="title">
                         <h1 class="font-weight-bold text-primary display-3">LEARN MAKE YOU HAPPY</h1>
                     </div>
@@ -82,7 +46,7 @@
                         <a href="<?= BURL ?>/auth/login" class="btn btn-primary font-weight-bold w-25">Get Started</a>
                     </div>
                 </div>
-                <div class="w-25">
+                <div class="w-25" data-aos="fade-left">
                     <img src="<?= BURL ?>/img /main.png" alt="" width="400px">
                 </div>
             </div>
@@ -93,7 +57,7 @@
         <!-- Will be happy with you -->
         <div class="bg-primary text-white p-5" id="willbehappywithyou">
             <div class="d-flex justify-content-around align-items-center h-100">
-                <div class="text-left w-50">
+                <div class="text-left w-50" data-aos="fade-right">
                     <div class="title">
                         <h1 class="font-weight-bold display-4">WILL BE HAPPY WITH YOU</h1>
                     </div>
@@ -103,14 +67,14 @@
                 </div>
                 <div class="container">
                     <div class="row mb-4">
-                        <div class="col">
+                        <div class="col" data-aos="fade-up">
                             <div class="card">
                                 <div class="card-img-container">
                                     <img src="<?= BURL ?>/img /s2.png" alt="" class="img-fluid">
                                 </div>
                             </div>
                         </div>
-                        <div class="col">
+                        <div class="col" data-aos="fade-up">
                             <div class="card">
                                 <div class="card-img-container">
                                     <img src="<?= BURL ?>/img /s3.png" alt="" class="img-fluid">
@@ -119,14 +83,14 @@
                         </div>
                     </div>
                     <div class="row mb-4">
-                        <div class="col">
+                        <div class="col" data-aos="fade-up">
                             <div class="card">
                                 <div class="card-img-container">
                                     <img src="<?= BURL ?>/img /s4.png" alt="" class="img-fluid">
                                 </div>
                             </div>
                         </div>
-                        <div class="col">
+                        <div class="col" data-aos="fade-up">
                             <div class="card">
                                 <div class="card-img-container">
                                     <img src="<?= BURL ?>/img /s1.png" alt="" class="img-fluid">
@@ -142,33 +106,33 @@
         <!-- HOW DO YOU FEEL STUDY WITH YOU -->
         <div class="my-5 p-3" id="howdoyoufeelstudywithyou">
             <div>
-                <div class="mb-3">
+                <div class="mb-3" data-aos="fade-down">
                     <h1 class="text-primary display-3 font-weight-bold text-center">HOW DO YOU FEEL <br> STUDY WITH YOU</h1>
                 </div>
                 <div>
                     <div class="row mb-4">
-                        <div class="col">
+                        <div class="col" data-aos="fade-up">
                             <div class="card">
                                 <div class="card-img-container">
                                     <img src="<?= BURL ?>/img /g1.png" alt="" class="img-fluid">
                                 </div>
                             </div>
                         </div>
-                        <div class="col">
+                        <div class="col" data-aos="fade-up">
                             <div class="card">
                                 <div class="card-img-container">
                                     <img src="<?= BURL ?>/img /g2.png" alt="" class="img-fluid">
                                 </div>
                             </div>
                         </div>
-                        <div class="col">
+                        <div class="col" data-aos="fade-up">
                             <div class="card">
                                 <div class="card-img-container">
                                     <img src="<?= BURL ?>/img /g3.png" alt="" class="img-fluid">
                                 </div>
                             </div>
                         </div>
-                        <div class="col">
+                        <div class="col" data-aos="fade-up">
                             <div class="card">
                                 <div class="card-img-container">
                                     <img src="<?= BURL ?>/img /g4.png" alt="" class="img-fluid">
@@ -177,28 +141,28 @@
                         </div>
                     </div>
                     <div class="row mb-4">
-                        <div class="col">
+                        <div class="col" data-aos="fade-up">
                             <div class="card">
                                 <div class="card-img-container">
                                     <img src="<?= BURL ?>/img /g5.png" alt="" class="img-fluid">
                                 </div>
                             </div>
                         </div>
-                        <div class="col">
+                        <div class="col" data-aos="fade-up">
                             <div class="card">
                                 <div class="card-img-container">
                                     <img src="<?= BURL ?>/img /g6.png" alt="" class="img-fluid">
                                 </div>
                             </div>
                         </div>
-                        <div class="col">
+                        <div class="col" data-aos="fade-up">
                             <div class="card">
                                 <div class="card-img-container">
                                     <img src="<?= BURL ?>/img /g7.png" alt="" class="img-fluid">
                                 </div>
                             </div>
                         </div>
-                        <div class="col">
+                        <div class="col" data-aos="fade-up">
                             <div class="card">
                                 <div class="card-img-container">
                                     <img src="<?= BURL ?>/img /g8.png" alt="" class="img-fluid">
@@ -214,12 +178,12 @@
         <!-- MEET,GROW UP AND GRADUATE TOGETHER -->
         <div class="bg-primary text-white p-3">
             <div class="d-flex justify-content-around align-items-center">
-                <div class="text-left w-50">
+                <div class="text-left w-50" data-aos="fade-right">
                     <div class="title">
                         <h1 class="font-weight-bold display-3">MEET,GROW UP AND GRADUATE TOGETHER</h1>
                     </div>
                 </div>
-                <div>
+                <div data-aos="fade-left">
                     <img src="<?= BURL ?>/img /f1.png" alt="" width="400px">
                 </div>
             </div>
